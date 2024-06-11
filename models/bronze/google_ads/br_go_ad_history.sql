429a74b0aa14602a4f4381ef452a65ba8a487be6
several27@icloud.com <several27@icloud.com>
2024-06-11 05:34:54 +0000

    Removes CTEs that will be filled out during the demos :)

--- models/bronze/google_ads/br_go_ad_history.sql
+++ models/bronze/google_ads/br_go_ad_history.sql
@@ -1,19 +1,3 @@
-WITH dt_go_ad_history AS (
-
-  SELECT * 
-  
-  FROM {{ ref('dt_go_ad_history')}}
-
-),
-
-Reformat_1 AS (
-
-  SELECT * 
-  
-  FROM dt_go_ad_history AS in0
-
-)
-
 SELECT *
 
 FROM Reformat_1
--- models/gold/all/gl_all_account_report.sql
+++ models/gold/all/gl_all_account_report.sql
@@ -1,26 +1,4 @@
-WITH sv_am_account_report AS (
-
-  SELECT * 
-  
-  FROM {{ ref('sv_am_account_report')}}
-
-),
-
-amazon_cleanup AS (
-
-  SELECT 
-    'amazon' AS source_relation,
-    profile_id AS account_id,
-    account_name,
-    total_clicks AS clicks,
-    total_impressions AS impressions,
-    total_cost AS cost
-  
-  FROM sv_am_account_report
-
-),
-
-sv_li_account_report AS (
+WITH sv_li_account_report AS (
 
   SELECT * 
   
@@ -28,62 +6,20 @@ sv_li_account_report AS (
 
 ),
 
-cleanup AS (
+cleanup_li AS (
 
+  {#Cleans up and organizes data from the sv_li_account_report table, including source relation, account ID, clicks, impressions, and cost.#}
   SELECT 
-    source_relation AS source_relation,
-    account_id AS account_id,
-    account_name,
-    clicks AS clicks,
-    impressions AS impressions,
-    cost AS cost
+    source_relation,
+    account_id,
+    clicks,
+    impressions,
+    cost
   
   FROM sv_li_account_report
-
-),
-
-sv_go_account_report AS (
-
-  SELECT * 
-  
-  FROM {{ ref('sv_go_account_report')}}
-
-),
-
-google_cleanup AS (
-
-  SELECT 
-    source_relation AS source_relation,
-    account_id AS account_id,
-    account_name,
-    TOTAL_CLICKS AS clicks,
-    TOTAL_IMPRESSIONS AS impressions,
-    TOTAL_SPEND AS cost
-  
-  FROM sv_go_account_report
-
-),
-
-combined_reports AS (
-
-  SELECT * 
-  
-  FROM cleanup
-  
-  UNION ALL
-  
-  SELECT * 
-  
-  FROM google_cleanup
-  
-  UNION ALL
-  
-  SELECT * 
-  
-  FROM amazon_cleanup
 
 )
 
 SELECT *
 
-FROM combined_reports
+FROM cleanup_li
